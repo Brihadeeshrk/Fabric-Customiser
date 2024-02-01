c30548c0847c212ab7f2f4d6d6cb3ca8da880e71
briha <hello@briha.xyz>
2024-02-01 14:08:32 +0530

    packages installed, fabric, recoil and chakra initialised

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,19 @@
 import "@/styles/globals.css";
+import { ChakraProvider } from "@chakra-ui/react";
 import type { AppProps } from "next/app";
+import { Toaster } from "react-hot-toast";
+import { RecoilRoot } from "recoil";
+import { theme } from "../chakra/theme";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <RecoilRoot>
+      <ChakraProvider theme={theme}>
+        <div>
+          <Toaster />
+        </div>
+        <Component {...pageProps} />
+      </ChakraProvider>
+    </RecoilRoot>
+  );
 }
