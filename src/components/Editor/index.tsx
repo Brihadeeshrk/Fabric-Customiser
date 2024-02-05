93ea7bf454091765276f20fe64c319a73c1f9fe3
briha <hello@briha.xyz>
2024-02-05 16:39:29 +0530

    fabric bg applied

--- src/components/Editor/index.tsx
+++ src/components/Editor/index.tsx
@@ -6,10 +6,10 @@ interface EditorProps {}
 const Editor: React.FC<EditorProps> = () => {
   return (
     <div className="flex">
-      <div className="border border-red-300 w-1/4 p-3">
+      <div className="w-1/4 p-3">
         <Menu />
       </div>
-      <div className="border border-blue-3000 w-3/4 p-3">Fabric Canvas</div>
+      <div className="w-3/4 p-3 bg-fabric-container">Fabric Canvas</div>
     </div>
   );
 };
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,7 +3,12 @@ import Editor from "@/components/Editor";
 export default function Home() {
   return (
     <div className="p-5">
-      <Editor />
+      <div className="hidden md:inline">
+        <Editor />
+      </div>
+      <div className="md:hidden">
+        <p>This editor is not designed to be used on mobile</p>
+      </div>
     </div>
   );
 }
