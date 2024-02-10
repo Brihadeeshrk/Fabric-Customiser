2e52cd4e7cd7a990f890a9dd082e1d8bfa683eaa
briha <hello@briha.xyz>
2024-02-10 23:23:17 +0530

    in the top menu, some buttons work

--- src/components/Menu/TopMenu/Tabs.tsx
+++ src/components/Menu/TopMenu/Tabs.tsx
@@ -14,51 +14,62 @@ import {
 } from "react-icons/lu";
 import { Button, Icon, Tooltip } from "@chakra-ui/react";
 import { Tab } from "../LeftMenu/Tabs";
-
-const TabOptions: Array<Tab> = [
-  {
-    title: "Copy",
-    icon: FaCopy,
-    onClick: () => {},
-  },
-  {
-    title: "Paste",
-    icon: FaPaste,
-    onClick: () => {},
-  },
-  {
-    title: "Horizontal Align",
-    icon: LuMoveHorizontal,
-    onClick: () => {},
-  },
-  {
-    title: "Vertical Align",
-    icon: LuMoveVertical,
-    onClick: () => {},
-  },
-  {
-    title: "Flip",
-    icon: LuFlipHorizontal2,
-    onClick: () => {},
-  },
-  {
-    title: "Lock Layer",
-    icon: FaLock,
-    onClick: () => {},
-  },
-  {
-    title: "Unlock Layer",
-    icon: FaLockOpen,
-    onClick: () => {},
-  },
-  {
-    title: "Empty Canvas",
-    icon: FaRegTrashAlt,
-    onClick: () => {},
-  },
-];
+import useMenuOps from "@/hooks/menuOps";
 
 const Tabs: React.FC = () => {
+  const {
+    copyObject,
+    pasteObject,
+    horizontalAlign,
+    verticalAlign,
+    flipObject,
+    lockLayer,
+    unlockLayer,
+    emptyCanvas,
+  } = useMenuOps();
+
+  const TabOptions: Array<Tab> = [
+    {
+      title: "Copy",
+      icon: FaCopy,
+      onClick: () => copyObject(),
+    },
+    {
+      title: "Paste",
+      icon: FaPaste,
+      onClick: () => pasteObject(),
+    },
+    {
+      title: "Horizontal Align",
+      icon: LuMoveHorizontal,
+      onClick: () => horizontalAlign(),
+    },
+    {
+      title: "Vertical Align",
+      icon: LuMoveVertical,
+      onClick: () => verticalAlign(),
+    },
+    {
+      title: "Flip",
+      icon: LuFlipHorizontal2,
+      onClick: () => flipObject(),
+    },
+    {
+      title: "Lock Layer",
+      icon: FaLock,
+      onClick: () => lockLayer(),
+    },
+    {
+      title: "Unlock Layer",
+      icon: FaLockOpen,
+      onClick: () => unlockLayer(),
+    },
+    {
+      title: "Empty Canvas",
+      icon: FaRegTrashAlt,
+      onClick: () => emptyCanvas(),
+    },
+  ];
   return (
     <div className="flex space-x-8">
       <div className="space-x-5 flex">
--- src/hooks/menuOps.tsx
+++ src/hooks/menuOps.tsx
@@ -1,11 +1,118 @@
-import React from "react";
+import { useContext } from "react";
 import { fabricContext } from "@/store/context";
 import { fabric } from "fabric";
-import { useContext } from "react";
+
+interface ExtendedCanvas extends fabric.Canvas {
+  clipboard?: fabric.Object[];
+}
 
 const useMenuOps = () => {
   const { canvas } = useContext(fabricContext);
 
-  return {};
+  const copyObject = () => {
+    if (canvas) {
+      const activeObject = canvas.getActiveObject();
+      if (activeObject) {
+        const objectsToCopy = canvas.getActiveObjects();
+        if (objectsToCopy && objectsToCopy.length > 0) {
+          (canvas as ExtendedCanvas).clipboard = objectsToCopy;
+        }
+      }
+    }
+  };
+
+  const pasteObject = () => {
+    if (canvas) {
+      const clipboard = (canvas as ExtendedCanvas).clipboard;
+      if (clipboard && clipboard.length > 0) {
+        clipboard.forEach((object) => {
+          const clonedObject = fabric.util.object.clone(object);
+          canvas?.add(clonedObject);
+        });
+        canvas?.discardActiveObject();
+        canvas?.renderAll();
+      }
+    }
+  };
+
+  const horizontalAlign = () => {
+    if (canvas) {
+      const activeObjects = canvas.getActiveObjects();
+      if (activeObjects && activeObjects.length > 1) {
+        const group = new fabric.Group(activeObjects);
+        if (group.left !== undefined && group.width !== undefined) {
+          const centerX = group.left + group.width / 2;
+          activeObjects.forEach((obj) => {
+            if (obj.width !== undefined) {
+              obj.left = centerX - obj.width / 2;
+            }
+          });
+          canvas?.requestRenderAll();
+        }
+      }
+    }
+  };
+
+  const verticalAlign = () => {
+    if (canvas) {
+      const activeObjects = canvas.getActiveObjects();
+      if (activeObjects && activeObjects.length > 1) {
+        const group = new fabric.Group(activeObjects);
+        if (group.top !== undefined && group.height !== undefined) {
+          const centerY = group.top + group.height / 2;
+          activeObjects.forEach((obj) => {
+            if (obj.height !== undefined) {
+              obj.top = centerY - obj.height / 2;
+            }
+          });
+          canvas?.requestRenderAll();
+        }
+      }
+    }
+  };
+
+  const flipObject = () => {
+    const activeObject = canvas?.getActiveObject();
+    if (activeObject) {
+      activeObject.flipX = !activeObject.flipX;
+      canvas?.requestRenderAll();
+    }
+  };
+
+  const lockLayer = () => {
+    if (canvas) {
+      canvas?.forEachObject((obj) => {
+        if (obj !== canvas.getActiveObject()) {
+          obj.selectable = false;
+        }
+      });
+      canvas?.requestRenderAll();
+    }
+  };
+
+  const unlockLayer = () => {
+    if (canvas) {
+      canvas?.forEachObject((obj) => {
+        obj.selectable = true;
+      });
+      canvas?.requestRenderAll();
+    }
+  };
+
+  const emptyCanvas = () => {
+    canvas?.clear();
+  };
+
+  return {
+    copyObject,
+    pasteObject,
+    horizontalAlign,
+    verticalAlign,
+    flipObject,
+    lockLayer,
+    unlockLayer,
+    emptyCanvas,
+  };
 };
+
 export default useMenuOps;
